fc92fceb0d1638688c423c20f905e8cbd504d1b4
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-11-19 04:28:51 +0000

    [Add] usersコントローラのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
+  get 'users/my_page' => 'users#mypage'
+  get '/users/unsubscribe' => 'users#unsubscribe'
+  patch '/users/withdraw' => 'users#withdraw'
+  get '/users/thanks' => 'users#thanks'
+  get '/users/search' => 'users#search'
+  resources :users, only: [:edit, :update, :show]
+
 
 end
