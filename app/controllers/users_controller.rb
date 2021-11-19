3590b5452af4619fac08172cb10fc428cade87d3
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-11-19 04:18:06 +0000

    [Add] usersコントローラ内のアクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def mypage
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+
+  def thanks
+  end
+
+  def search
+  end
+
+  def show
+  end
+
 end
