3ebdb90a53c7d587ff5c4934a02fdb8a2c48ad0e
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-11-21 07:30:49 +0000

    [Add] アイテム新規登録機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,20 @@
 class ItemsController < ApplicationController
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    date = params.require(:item).permit(:expiration_date)
+    expiration_date = Date.parse( date["expiration_date(1i)"] + "-" + date["expiration_date(2i)"] + "-" + date["expiration_date(3i)"] )
+    @item = Item.new(item_params.merge(expiration_date: expiration_date))
+    @item.user_id = current_user.id
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -24,7 +35,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :amount, :expiration_date)
+    params.require(:item).permit(:name, :amount, :category_id)
   end
 
 end
