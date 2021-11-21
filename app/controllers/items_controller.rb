9d39677208165381360f15edbaca0c2754db69a0
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-11-21 09:34:15 +0000

    [Fix] itemsコントローラのcreateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,14 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     date = params.require(:item).permit(:expiration_date)
-    expiration_date = Date.parse( date["expiration_date(1i)"] + "-" + date["expiration_date(2i)"] + "-" + date["expiration_date(3i)"] )
-    @item = Item.new(item_params.merge(expiration_date: expiration_date))
-    @item.user_id = current_user.id
+    # year month day hitotudemo kara deareba true!
+    if params[:item]["expiration_date(1i)"].blank? || params[:item]["expiration_date(2i)"].blank? || params[:item]["expiration_date(3i)"].blank?
+      @item.user_id = current_user.id
+    else # subete karajanakereba
+      expiration_date = Date.parse( date["expiration_date(1i)"] + "-" + date["expiration_date(2i)"] + "-" + date["expiration_date(3i)"] )
+      @item = Item.new(item_params.merge(expiration_date: expiration_date))
+      @item.user_id = current_user.id
+    end
     if @item.save
       redirect_to item_path(@item.id)
     else
