5e66bf60162fbbb42477970d19c8b24ce4367b6e
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-11-21 13:02:09 +0000

    [Add] アイテム情報更新機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,23 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    date = params.require(:item).permit(:expiration_date)
+    # year month day hitotudemo kara deareba true!
+    if params[:item]["expiration_date(1i)"].blank? || params[:item]["expiration_date(2i)"].blank? || params[:item]["expiration_date(3i)"].blank?
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        render :edit
+      end
+    else # subete karajanakereba
+      expiration_date = Date.parse( date["expiration_date(1i)"] + "-" + date["expiration_date(2i)"] + "-" + date["expiration_date(3i)"] )
+      if @item.update(item_params.merge(expiration_date: expiration_date))
+        redirect_to item_path(@item.id)
+      else
+        render :edit
+      end
+    end
   end
 
   def destroy
