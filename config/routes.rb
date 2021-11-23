b457ea2364792fdb2b310f698cb0a2cbeadc4c91
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-11-23 09:11:27 +0000

    [Add] relationshipsコントローラとルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'homes#top'
-  get 'users/my_page' => 'users#mypage'
+  get '/users/my_page' => 'users#mypage'
   get '/users/unsubscribe' => 'users#unsubscribe'
   patch '/users/withdraw' => 'users#withdraw'
   get '/users/thanks' => 'users#thanks'
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :show]
   resources :categories, only: [:index]
   resources :items
+  resource :relationships, only: [:create, :destroy]
+  get '/relationships/followings' => 'relationships#followings'
+  get '/relationships/followers' => 'relationships#followers'
 
 end
