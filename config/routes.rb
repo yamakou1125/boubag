822b26bfe478aaf1ff6895027657f8f4a729d5e1
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-12-03 05:45:21 +0000

    [Fix] users/destroyアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/users/my_page' => 'users#mypage'
   get '/users/unsubscribe' => 'users#unsubscribe'
-  patch '/users/withdraw' => 'users#withdraw'
   get '/users/thanks' => 'users#thanks'
   get '/users/search' => 'users#search'
-  resources :users, only: [:edit, :update, :show] do
+  resources :users, only: [:edit, :update, :destroy, :show] do
     resource :relationships, only: [:create, :update, :destroy]
     get 'followings' => 'relationships#followings'
     get 'followers' => 'relationships#followers'
