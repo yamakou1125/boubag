db2d0e29a114c29338f8d1a4d5bbbad4d0c3ee1b
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-12-07 07:48:09 +0000

    [Update] mailerの各viewのレイアウト

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,13 +3,13 @@ class NotificationMailer < ApplicationMailer
   def send_first_notice(item)
     @item = item
     @user = item.user
-    mail to: @user.email, subject: '【boubag】 防災アイテム交換のお知らせ'
+    mail to: @user.email, subject: '【BouBag】 防災アイテム交換のお知らせ'
   end
 
   def send_last_notice(item)
     @item = item
     @user = item.user
-    mail to: @user.email, subject: '【boubag】 防災アイテム交換のお知らせ'
+    mail to: @user.email, subject: '【BouBag】 防災アイテム交換のお知らせ'
   end
 
 end
