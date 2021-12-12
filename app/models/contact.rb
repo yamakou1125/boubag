99557cc0f3572344736eddbc8d1a596893744f26
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-12-12 07:31:54 +0000

    [Update] コンタクトモデルのメアドカラムのバリデーションにformatを追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :message, presence:true
 
 end
