df6ac6715e912606c910d12b0513f5c59b1bbc73
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-12-13 08:06:38 +0000

    [Add] アイテム詳細画面とアイテム編集画面へのアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,11 +26,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      @item = item
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
-    @item = Item.find(params[:id])
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      @item = item
+    else
+      redirect_to root_path
+    end
   end
 
   def update
