eaf2a9d77dc58f426d9a05cfdc5b6fc8267f49c4
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-12-13 08:17:21 +0000

    [Add] フォロワー画面へのアクセス制限

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -27,9 +27,13 @@ class RelationshipsController < ApplicationController
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @unapproved = user.reverse_of_relationships.where(status: 0)
-    @allows = user.reverse_of_relationships.where(status: 1)
+    if params["user_id"].to_i == current_user.id
+      user = User.find(params[:user_id])
+      @unapproved = user.reverse_of_relationships.where(status: 0)
+      @allows = user.reverse_of_relationships.where(status: 1)
+    else
+      redirect_to root_path
+    end
   end
 
 end
