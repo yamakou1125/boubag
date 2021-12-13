b783afe56ba012be94438366392cbed40b3ab0a2
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-12-13 10:44:07 +0000

    [Add] ユーザ登録、ログイン、プロフィール編集の成功時にフラッシュメッセージを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "更新が完了しました。"
       redirect_to users_my_page_path
     else
       render :edit
